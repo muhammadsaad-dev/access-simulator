239f9df00286c2c9170f6e461b13531f078d062e
Muhammad Saad <saad.oct2003@gmail.com>
2025-09-20 10:23:11 +0500

    fixed lastVisited Bug

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { auth } from "./utils.js"
+import { readJSON, stringToTime } from "./utils.js"
 import cors from "cors"
 
 const app = express()
@@ -9,15 +9,52 @@ const PORT = 3000
 app.use(express.json())
 app.use(cors())
 
-// Simple route
-app.get("/", (req, res) => {
-  res.send("Access Simulator Backend Running 🚀")
-})
+let roomRules = await readJSON("./roomRules.json")
+
+let lastVisited = {}
+
+function auth(user) {
+  const room = user.room
+  let result = { [user.id]: [] }
+
+  if (user.access_level < roomRules[room].minAccessLevel) {
+    result[user.id].push(
+      `ACCESS DENIED: ${user.id}'s access level is low for the requested room.`
+    )
+  }
+  let requestTime = stringToTime(user.request_time)
+  let openTime = stringToTime(roomRules[room].openTime)
+  let closeTime = stringToTime(roomRules[room].closeTime)
+  let cooldownTime = roomRules[room].cooldownTime
+
+  if (requestTime < openTime || requestTime > closeTime) {
+    result[user.id].push("ACCESS DENIED: Requested room is closed right now.")
+  }
+
+  let record = lastVisited[`${user.id}_${room}`]
+  if (record) {
+    let diff = (requestTime - record) / (1000 * 60)
+    if (diff < cooldownTime) {
+      result[user.id].push(
+        `ACCESS DENIED: Cooldown period is still active, Try again in ${
+          cooldownTime - diff
+        } minutes`
+      )
+    }
+  }
+  lastVisited[`${user.id}_${room}`] = requestTime
+
+  if (result[user.id].length == 0) {
+    result[user.id].push(`ACCESS GRANTED: All conditions are fulfilled.`)
+  }
+
+  return result
+}
 
 // Simulation endpoint (we'll plug in your logic here later)
 app.post("/simulate", (req, res) => {
   const employees = req.body // array of employees
-
+  lastVisited = {}
   let results = employees.map((employee) => auth(employee))
   res.json({ message: "Simulation received", results })
 })
--- backend/utils.js
+++ backend/utils.js
@@ -1,6 +1,6 @@
 import { readFile } from "fs/promises"
 
-function stringToTime(timeString) {
+export function stringToTime(timeString) {
   // Split the string into hours and minutes
   const [hours, minutes] = timeString.split(":").map(Number)
 
@@ -11,7 +11,7 @@ function stringToTime(timeString) {
   return date
 }
 
-async function readJSON(filePath) {
+export async function readJSON(filePath) {
   try {
     const jsonString = await readFile(filePath, "utf8")
     return JSON.parse(jsonString)
@@ -19,45 +19,4 @@ async function readJSON(filePath) {
     console.error("Error reading JSON file:", err)
     return null
   }
-}
-
-let roomRules = await readJSON("./roomRules.json")
-let lastVisited = {}
-
-export function auth(user) {
-  const room = user.room
-  let result = { [user.id]: [] }
-
-  if (user.access_level < roomRules[room].minAccessLevel) {
-    result[user.id].push(
-      `ACCESS DENIED: ${user.id}'s access level is low for the requested room.`
-    )
-  }
-  let requestTime = stringToTime(user.request_time)
-  let openTime = stringToTime(roomRules[room].openTime)
-  let closeTime = stringToTime(roomRules[room].closeTime)
-  let cooldownTime = roomRules[room].cooldownTime
-
-  if (requestTime < openTime || requestTime > closeTime) {
-    result[user.id].push("ACCESS DENIED: Requested room is closed right now.")
-  }
-
-  let record = lastVisited[`${user.id}_${room}`]
-  if (record) {
-    let diff = (requestTime - record) / (1000 * 60)
-    if (diff < cooldownTime) {
-      result[user.id].push(
-        `ACCESS DENIED: Cooldown period is still active, Try again in ${
-          cooldownTime - diff
-        } minutes`
-      )
-    }
-  }
-  lastVisited[`${user.id}_${room}`] = requestTime
-
-  if (result[user.id].length == 0) {
-    result[user.id].push(`ACCESS GRANTED: All conditions are fulfilled.`)
-  }
-
-  return result
 }
